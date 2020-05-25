df68708282d0e4e6c8a5ea2fe041459dcc194310
Pyro-YT <sikling247@gmail.com>
2020-05-25 14:15:56 +0100

    asd

---
+++ bloxapi/utils/classes.py
@@ -0,0 +1,10 @@
+class item:
+    """
+    Represents an item.
+    """
+    def __init__(self, name: str, id: int, price: int, rap: int):
+
+        self.name = name
+        self.id = id
+        self.price = price
+        self.rap = rap
