7ca209b4570c3331f2d6d2ef9fdaab4c3dd83c2e
Pyro-YT <sikling247@gmail.com>
2020-05-25 19:30:44 +0100

    get_item_description

--- bloxapi/item.py
+++ bloxapi/item.py
@@ -1,6 +1,7 @@
 import json
 import asyncio
 import requests
+from bs4 import BeautifulSoup
 
 
 class Item:
@@ -9,3 +10,16 @@ class Item:
 
         self.name = name
         self.id = id
+
+
+    async def get_item_price(self):
+        response = requests.get(url=f'https://catalog.roblox.com/v1/search/items/details?id={self.id}')
+        json = response.json()["data"][0]
+        return json["price"]
+
+    async def get_item_description(self):
+        response = requests.get(url=f'https://catalog.roblox.com/v1/search/items/details?id={self.id}')
+        json = response.json()["data"][0]
+        return json["description"]
+
+    
